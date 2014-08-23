6a7c307df881f1299c60b39a3d581dd059f1f2f9
Fabio <fabiomassimomilano@gmail.com>
2014-08-23 20:11:14 +0200

    Added IXLocation file

---
+++ EuropeanaApp/EuropeanaApp/Classes/Model/IXLocation.h
@@ -0,0 +1,13 @@
+//
+//  IXLocation.h
+//  EuropeanaApp
+//
+//  Created by Fabio Milano on 8/23/14.
+//  Copyright (c) 2014 Phluxus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IXLocation : NSObject
+
+@end
---
+++ EuropeanaApp/EuropeanaApp/Classes/Model/IXLocation.m
@@ -0,0 +1,13 @@
+//
+//  IXLocation.m
+//  EuropeanaApp
+//
+//  Created by Fabio Milano on 8/23/14.
+//  Copyright (c) 2014 Phluxus. All rights reserved.
+//
+
+#import "IXLocation.h"
+
+@implementation IXLocation
+
+@end
