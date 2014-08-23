04fa3159b21b95b1401a7e8770b5715043c18d9a
Axel Roest <github@phlux.us>
2014-08-23 22:10:19 +0200

    created imageassets.

--- EuropeanaApp/EuropeanaAppTests/EuropeanaAppTests.m
+++ EuropeanaApp/EuropeanaAppTests/EuropeanaAppTests.m
@@ -7,9 +7,14 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "IXManager.h"
+#import "IXLocationManager.h"
+#import "IXBeacon.h"
 
 @interface EuropeanaAppTests : XCTestCase
-
+@property (nonatomic, strong) IXManager *manager;
+@property (nonatomic, strong) IXLocationManager *locationManager;
+@property (nonatomic, strong) IXBeacon *ixBeacon;
 @end
 
 @implementation EuropeanaAppTests
@@ -17,6 +22,9 @@
 - (void)setUp
 {
     [super setUp];
+    self.ixBeacon = [[IXBeacon alloc] init];
+    self.manager = [[IXManager alloc] init];
+    self.locationManager = [[IXLocationManager alloc] initWithDelegate:self.manager];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
@@ -26,8 +34,12 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
+- (void)testBeacon
+//
+{// - (void) ixLocationManager : (IXLocationManager *)ixLocationManager spottedIXBeacon:(IXBeacon *) ixBeacon
+
+    [self.manager ixLocationManager:nil enteredIXBeaconRegion:self.ixBeacon];
+    
     XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
