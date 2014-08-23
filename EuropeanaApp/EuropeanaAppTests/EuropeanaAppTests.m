529db2a4b7e3ca16185f6536ea9c241166d9c5d4
Axel Roest <github@phlux.us>
2014-08-23 22:44:35 +0200

    Added unit test (yeah!) for testing the beacon notification to audio file.

--- EuropeanaApp/EuropeanaAppTests/EuropeanaAppTests.m
+++ EuropeanaApp/EuropeanaAppTests/EuropeanaAppTests.m
@@ -10,11 +10,13 @@
 #import "IXManager.h"
 #import "IXLocationManager.h"
 #import "IXBeacon.h"
+#import "IXAudioManager.h"
 
 @interface EuropeanaAppTests : XCTestCase
 @property (nonatomic, strong) IXManager *manager;
 @property (nonatomic, strong) IXLocationManager *locationManager;
 @property (nonatomic, strong) IXBeacon *ixBeacon;
+@property (nonatomic, strong) IXAudioManager *ixAudioManager;
 @end
 
 @implementation EuropeanaAppTests
@@ -24,7 +26,8 @@
     [super setUp];
     self.ixBeacon = [[IXBeacon alloc] init];
     self.manager = [[IXManager alloc] init];
-    self.locationManager = [[IXLocationManager alloc] initWithDelegate:self.manager];
+//    self.locationManager = [[IXLocationManager alloc] initWithDelegate:self.manager];
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
@@ -38,9 +41,8 @@
 //
 {// - (void) ixLocationManager : (IXLocationManager *)ixLocationManager spottedIXBeacon:(IXBeacon *) ixBeacon
 
-    [self.manager ixLocationManager:nil enteredIXBeaconRegion:self.ixBeacon];
+    [self.manager ixLocationManager:nil spottedIXBeacon:self.ixBeacon];
     
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
