f9a3d39801039934b22eba19732b9a49bc325ea6
Axel Roest <github@phlux.us>
2014-08-23 19:50:29 +0200

    Added manager

---
+++ EuropeanaApp/EuropeanaApp/IXManager.h
@@ -0,0 +1,13 @@
+//
+//  IXManager.h
+//  EuropeanaApp
+//
+//  Created by Axel Roest on 23-08-14.
+//  Copyright (c) 2014 Phluxus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IXManager : NSObject
+
+@end
---
+++ EuropeanaApp/EuropeanaApp/IXManager.m
@@ -0,0 +1,13 @@
+//
+//  IXManager.m
+//  EuropeanaApp
+//
+//  Created by Axel Roest on 23-08-14.
+//  Copyright (c) 2014 Phluxus. All rights reserved.
+//
+
+#import "IXManager.h"
+
+@implementation IXManager
+
+@end
