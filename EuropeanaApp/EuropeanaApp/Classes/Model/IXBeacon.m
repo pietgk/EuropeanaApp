90b6ffb7b678131bddf2335df9bc2968461533fe
Fabio <fabiomassimomilano@gmail.com>
2014-08-24 10:18:57 +0200

    IXLocation manager first implementation.

--- EuropeanaApp/EuropeanaApp/Classes/Model/IXBeacon.m
+++ EuropeanaApp/EuropeanaApp/Classes/Model/IXBeacon.m
@@ -10,4 +10,29 @@
 
 @implementation IXBeacon
 
++ (instancetype) createWithUUID:(NSString *)identifier major:(NSUInteger)major minor:(NSUInteger)minor distance:(CGFloat) distance;
+{
+    return [[self alloc]initWithIdentifier:identifier major:major minor:minor distance:distance];
+}
+
+- (instancetype) initWithIdentifier:(NSString *)identifier major:(NSUInteger)major minor:(NSUInteger)minor distance:(CGFloat) distance
+{
+    self = [super init];
+    
+    if (self) {
+        self.identifier = identifier;
+        self.major = major;
+        self.minor = minor;
+        self.distance = distance; //OR convenience algorithm to improve distance accuracy (the one provided by Apple is a little bit shitty)
+    }
+    
+    return self;
+}
+
+- (CGFloat)realDistance
+{
+    // New distance's algotrithm
+    return -1.0f;
+}
+
 @end
