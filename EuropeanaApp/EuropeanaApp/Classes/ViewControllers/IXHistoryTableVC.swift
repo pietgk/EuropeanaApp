54790706ecac8cd27d56a46f9409d1c6b31859f1
Axel Roest <github@phlux.us>
2016-03-23 14:49:11 +0100

    fixed historicpoi display

--- EuropeanaApp/EuropeanaApp/Classes/ViewControllers/IXHistoryTableVC.swift
+++ EuropeanaApp/EuropeanaApp/Classes/ViewControllers/IXHistoryTableVC.swift
@@ -22,12 +22,17 @@ class IXHistoryTableVC: UITableViewController {
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-        data = IXData.sharedData()
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        data = IXData.sharedData()
+        self.tableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
