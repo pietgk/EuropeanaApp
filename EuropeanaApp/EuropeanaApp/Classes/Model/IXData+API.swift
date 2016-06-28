2da4d09fba94865feeefc692421ebd9f15613bc1
Axel Roest <axel@phlux.us>
2016-06-28 12:48:44 +0200

    Added data+api

---
+++ EuropeanaApp/EuropeanaApp/Classes/Model/IXData+API.swift
@@ -0,0 +1,18 @@
+//
+//  IXData+API.swift
+//  ArtWhisper
+//
+//  Created by Axel Roest on 20/05/16.
+//  Copyright © 2016 Phluxus. All rights reserved.
+//
+
+import Foundation
+
+/*
+ First, try to get some data from the API, then make it nicer in NSOperations elsewhere
+ Because of missing API, let's focus on firebase for now.
+ 
+ */
+extension IXData {
+
+}
